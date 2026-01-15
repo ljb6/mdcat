90e52e9376f13bb7c40d19b33c55142b25d3db11
Lucca <luccajbecker@gmail.com>
2026-01-14 23:19:17 -0300

    feat: add initial CLI skeleton

--- mdcat.c
+++ mdcat.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
+void process_file(const char *filename)
 {
-  return 0;
+  printf("Filename: %s\n", filename);
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
+    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  process_file(argv[1]);
+
+  return EXIT_SUCCESS;
 }
