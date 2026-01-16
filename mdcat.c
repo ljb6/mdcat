1979538e4caae785b6d289667ba22218498f6a25
Lucca <luccajbecker@gmail.com>
2026-01-16 20:46:06 -0300

    feat: add process_fptr

--- mdcat.c
+++ mdcat.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MAX_LINE_LENGTH 4096
+
+void process_line(const char *line)
+{
+}
+
+int process_fptr(FILE *fptr)
+{
+  char line[MAX_LINE_LENGTH];
+
+  while (fgets(line, sizeof(line), fptr))
+  {
+    size_t line_len = strlen(line);
+
+    if (line_len > 0 && line[line_len - 1] != '\n' && !feof(fptr))
+    {
+      fprintf(stderr, "Error: file has lines too long\n");
+      return EXIT_FAILURE;
+    }
+
+    process_line(line);
+  }
+
+  return EXIT_SUCCESS;
+}
+
 int process_file(const char *filename)
 {
   FILE *fptr = fopen(filename, "r");
@@ -16,6 +42,8 @@ int process_file(const char *filename)
   {
     fprintf(stderr, "Invalid file format. Use a .md file\n");
   }
+
+  process_fptr(fptr);
 
   fclose(fptr);
   return EXIT_SUCCESS;
